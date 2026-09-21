e13649c27b66ede6b199e58e364f025ac2b1b37c
mnedel-ops <m.nedel@ufn.edu.br>
2026-09-21 15:37:18 -0300

    Caminhos acertados

--- Entities/User Interface/ui_arena.gd
+++ Entities/User Interface/ui_arena.gd
@@ -1,8 +1,8 @@
 extends Control
 
-@export var arena_select : PackedScene
+@export var match_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _input(event: InputEvent) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		get_tree().change_scene_to_packed(arena_select)
+		get_tree().change_scene_to_packed(match_scene)
